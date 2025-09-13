8770333cdb6bda2801b5095b987863d53345fda7
Valentin Sebastien <sebastien.kronlund@gmail.com>
2025-09-13 21:55:56 +0200

    Breaking out into separate repo

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
-import summonersTerminal.SummonersTerminal;
 
 public class Main {
     public static void main(String[] args) {
-        SummonersTerminal game = new SummonersTerminal();
-        game.PlayGame();
-        return;
     }
 
 }
