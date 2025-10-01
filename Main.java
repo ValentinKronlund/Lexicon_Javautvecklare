f29ad4b11f083e787ff5b4d7b7e62304b12dcfae
Valentin Sebastien <sebastien.kronlund@gmail.com>
2025-10-01 14:50:47 +0200

    Empty

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    String firstName;
+    String lastName;
+
+    public Main(Map<String, String> constructorData) {
+        firstName = constructorData.get("firstName");
+        firstName = constructorData.get("lastName");
+    }
+
     public static void main(String[] args) {
+        Map<String, String> constructorData = new HashMap<String, String>() {
+            {
+                put("firstName", "Alex");
+                put("lastName", "Armstrong");
+            }
+        };
+
+        Main myObj = new Main(constructorData);
     }
 
 }
